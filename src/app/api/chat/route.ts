71034dfa7d38a343e4c2b7c26d68239cd9d81b85
Teinturier <henri.teinturier@gmail.com>
2024-04-08 13:43:41 +0200

    fix keep all userPrompt for embedding research

--- src/app/api/chat/route.ts
+++ src/app/api/chat/route.ts
@@ -9,7 +9,7 @@ import { format } from "path";
 export const runtime = "edge";
 
 const SYSTEM_MESSAGE = `
-  Context:
+  Your role:
   You are NextJS Docs GPT, a chatbot that know up to date information about NextJS documentation.
   Your task is to create simple, easy to understand, responses to questions about NextJS.
   You are good in pedagogy and can explain complex topics in simple terms.
@@ -39,17 +39,25 @@ export async function POST(req: Request) {
   // Extract the `messages` from the body of the request
   const { messages } = (await req.json()) as { messages: Message[] };
 
+  const userPromptHistoric = messages.filter((m) => m.role === "user");
+  const userPromptHistoricContent = userPromptHistoric
+    .map((m) => m.content)
+    .toString();
+
   const lastMessage = messages[messages.length - 1];
   const userPrompt = lastMessage.content;
 
   try {
     const response = await openai.embeddings.create({
       model: "text-embedding-3-small",
-      input: userPrompt,
+      input: userPromptHistoricContent,
       encoding_format: "float",
     });
 
     const promptEmbedding = response.data[0].embedding;
+    const promptEmbeddingFormatted = promptEmbedding
+      .toString()
+      .replace(/\.\.\./g, "");
 
     const sql = getNeon();
 
@@ -57,16 +65,16 @@ export async function POST(req: Request) {
     SELECT text, file_path
     FROM (
       SELECT text, n_tokens, embeddings, file_path,
-      (embeddings <=> '[${promptEmbedding}]') AS distances,
-      SUM(n_tokens) OVER (ORDER BY (embeddings <=> '[${promptEmbedding}]')) as cum_n_tokens
+      (embeddings <=> '[${promptEmbeddingFormatted}]') AS distances,
+      SUM(n_tokens) OVER (ORDER BY (embeddings <=> '[${promptEmbeddingFormatted}]')) as cum_n_tokens
       FROM documents
     ) subquery
     WHERE cum_n_tokens <= $1
     ORDER BY distances ASC;
   `;
 
-    const MAX_TOKENS = 10000;
-    const queryParams = [2500];
+    const MAX_TOKENS = 2500;
+    const queryParams = [MAX_TOKENS];
 
     const result = (await sql(insertQuery, queryParams)) as {
       text: string;
@@ -84,7 +92,7 @@ export async function POST(req: Request) {
     });
 
     const context = formattedResult.map((r) => {
-      return `${r.url}: ${r.content}`;
+      return `url: ${r.url}: "\n"context:  ${r.content}`;
     });
 
     const otherMessages = messages.slice(0, messages.length - 1).map((m) => {
@@ -120,7 +128,6 @@ export async function POST(req: Request) {
       model: "gpt-3.5-turbo-0125",
       stream: true,
       max_tokens: 1000,
-      // messages: [{ role: "user", content: userPrompt }],
       messages: finalMessages,
     });
 
